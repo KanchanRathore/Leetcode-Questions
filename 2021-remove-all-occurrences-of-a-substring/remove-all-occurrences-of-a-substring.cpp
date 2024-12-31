8bd1c0db194ea341b112859fb953ba72d24eee98
KANCHAN RATHORE <119852360+KanchanRathore@users.noreply.github.com>
2024-12-31 21:04:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (42.65%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int n = s.length();
-         for(int i = 0;i<=n-1;i++)
-         {
+       while(s.find(part) != std::string::npos)
+       {
             int d = s.find(part);
             if(d != std::string::npos)
             {
                 s.replace(d,part.length(),"");
             }
-         }
+       }
+        
          return s;
     }
 };
