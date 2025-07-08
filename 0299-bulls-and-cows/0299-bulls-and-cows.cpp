3d2c5ed1ec349df2057b0f8c187dbc3a8aadd7fd
KANCHAN RATHORE <119852360+KanchanRathore@users.noreply.github.com>
2025-07-08 23:57:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (48.37%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -3,21 +3,27 @@ public:
     string getHint(string secret, string guess) {
         int count1 = 0;
         int count2 = 0;
+        unordered_map<int,int>a;
+        for(auto & ch : secret)
+        {
+            a[ch]++;
+        }
         for(int i =0; i<=secret.length()-1; i++)
         {
            if(secret[i] == guess[i])
            {
                count1++;
-           }
-           else if (guess.find(secret[i]) != -1)
-           {
-               count2++;
-           }
-           else
-           {
-            i++;
+               a[secret[i]]--;
            }
         }
+           for(int i =0; i<=guess.length()-1; i++)
+           {
+              if(a[guess[i]] >0 && secret[i]!=guess[i])
+              {
+                count2++;
+                a[guess[i]]--;
+              }
+           }
        string ans = to_string(count1)+"A"+to_string(count2)+"B";
        return ans;
     }
