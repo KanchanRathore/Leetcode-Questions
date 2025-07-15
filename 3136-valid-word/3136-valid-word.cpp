5e9889cdf68eb21090199c149c86fc295312392a
KANCHAN RATHORE <119852360+KanchanRathore@users.noreply.github.com>
2025-07-15 23:39:03 +0530

    Time: 1 ms (16.26%), Space: 8 MB (67.48%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    bool check(string word)
-    {
-         for(char c: word)
-        {
-            if(isalpha(c))
-            {
-                return true;
-            }
-        }
-        return false;
-         
-    }
     bool isValid(string word) {
         int vowelcount = 0;
         int consonantcount = 0;
@@ -26,15 +14,23 @@ public:
         }
         else
         {
-                if(word.find('a') != -1 || word.find('A') != -1  || word.find('e') != -1 || word.find('E') != -1 || word.find('o') != -1 || word.find('O') != -1 || word.find('u') != -1 ||word.find('U') != -1 ||  word.find('i') != -1 || word.find('I') != -1)
+            for(char ch : word)
+            {
+                if(isdigit(ch))
+                {
+                    continue;
+                }
+                int low = tolower(ch);
+                if(low == 'a' || low =='e' || low == 'i' || low == 'o' || low == 'u')
                 {
                     vowelcount++;
                 }
-                if((word.find('a') == -1 || word.find('A') == -1  || word.find('e') == -1 || word.find('E') == -1 || word.find('o') == -1 || word.find('O') == -1 || word.find('u') == -1 ||word.find('U') == -1 ||  word.find('i') == -1 || word.find('I') == -1) && check(word))
+                else if(isalpha(ch))
                 {
                     consonantcount++;
                 }
         } 
+        }
         cout<<vowelcount<<endl;
         if(vowelcount >0 && consonantcount > 0)
         {
@@ -44,6 +40,7 @@ public:
         {
           return false;
         }
+      
 
     }
 };
