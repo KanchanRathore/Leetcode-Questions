656f717d431da1b2542edd82e0be777c4d4791ad
KANCHAN RATHORE <119852360+KanchanRathore@users.noreply.github.com>
2025-08-14 21:09:13 +0530

    Time: 2 ms (16%), Space: 9.1 MB (52.45%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -3,21 +3,16 @@ public:
     string largestGoodInteger(string num) {
         string ans = "";
         int max = -1;
-        for(int i = 0; i <= num.length()-3; i++)
-        {
-            if(num[i] == num[i+1] && num[i] == num[i+2]) 
-            {
+        for (int i = 0; i <= num.length() - 3; i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
                 int val = num[i] - '0';
-                if(val > max)
-                {
-                   max = val;
-                   ans = num.substr(i,3);
+                if (val > max) {
+                    max = val;
+                    ans = num.substr(i, 3);
                 }
-                
             }
         }
-       
-        return ans;
 
+        return ans;
     }
 };
