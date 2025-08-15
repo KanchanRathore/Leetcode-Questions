f15101b3058571e5e976945ec0b8581e4c255cc4
KANCHAN RATHORE <119852360+KanchanRathore@users.noreply.github.com>
2025-08-15 14:40:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n ==1)
+        {
+            return true;
+        }
+        if(n <= 0)
+        {
+          return false;
+        }
+        int g = 1;
+        while(n != 1)
+        {
+            int n1 = n%4;
+            if(n1 == 0 && (n/4 == 4 || n/4 == 1))
+            {
+                n = n/4;
+            }
+            else
+            {
+              return false;
+            }
+        }
+        return true;
+    }
+};
